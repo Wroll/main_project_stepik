02fa336da20bcde829195a63bc250b7ca7385799
Wroll <DM.shev4ik@gmail.com>
2020-01-02 01:27:48 +0200

    wrote some test for login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,15 +1,44 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+import pytest
 
-LINK = "http://selenium1py.pythonanywhere.com/"
+LINK = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+LINK_TO_LOGIN_PAGE = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
 
 
+@pytest.mark.main_page
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, LINK)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
 
 
+@pytest.mark.main_page
 def test_guest_should_see_login_link(browser):
     page = MainPage(browser, LINK)
     page.open()
     page.should_be_login_link()
+
+
+# pytest -v --tb=line -m "login_page" --language=en test_main_page.py
+
+@pytest.mark.login_page
+def test_correctness_login_form(browser):
+    page = LoginPage(browser, LINK_TO_LOGIN_PAGE)
+    page.open()
+    page.should_be_login_form()
+
+
+@pytest.mark.login_page
+def test_correctness_register_form(browser):
+    page = LoginPage(browser, LINK_TO_LOGIN_PAGE)
+    page.open()
+    page.should_be_register_form()
+
+
+@pytest.mark.login_page
+def test_correctness_login_url(browser):
+    page = LoginPage(browser, LINK_TO_LOGIN_PAGE)
+    page.open()
+    page.should_be_login_url()
+
