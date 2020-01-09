2c14cb75f026e5d56790fa174cd5123b15330b4d
Wroll <DM.shev4ik@gmail.com>
2020-01-09 21:19:47 +0200

    Refactored tests

--- test_main_page.py
+++ test_main_page.py
@@ -1,19 +1,19 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
-from .pages.product_page import ProductPage
-import pytest
 import time
+
+# pytest -v --tb=line -m "login_page" --language=en test_main_page.py
+
 
 LINK = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
 LINK_TO_LOGIN_PAGE = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+MAIN_LINK = "http://selenium1py.pythonanywhere.com"
 
 
-@pytest.mark.login_guest
 class TestLoginFromMainPage:
     def test_guest_can_go_to_login_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com"
-        page = MainPage(browser, link)
+        page = MainPage(browser, MAIN_LINK)
         page.open()
         page.go_to_login_page()
         login_page = LoginPage(browser, browser.current_url)
@@ -26,44 +26,26 @@ class TestLoginFromMainPage:
         page.should_be_login_link()
 
 
-# pytest -v --tb=line -m "login_page" --language=en test_main_page.py
-
-@pytest.mark.login_page
 def test_correctness_login_form(browser):
     page = LoginPage(browser, LINK_TO_LOGIN_PAGE)
     page.open()
     page.should_be_login_form()
 
 
-@pytest.mark.login_page
 def test_correctness_register_form(browser):
     page = LoginPage(browser, LINK_TO_LOGIN_PAGE)
     page.open()
     page.should_be_register_form()
 
 
-@pytest.mark.login_page
 def test_correctness_login_url(browser):
     page = LoginPage(browser, LINK_TO_LOGIN_PAGE)
     page.open()
     page.should_be_login_url()
 
 
-@pytest.mark.basket_page
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/"
-    page = BasketPage(browser, link)
-    page.open()
-    page.should_be_basket_link()
-    page.go_to_basket_view_page()
-    page.check_items_in_basket()
-    page.check_text_in_empty_basket()
-
-
-@pytest.mark.basket_page
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = BasketPage(browser, link)
+    page = BasketPage(browser, MAIN_LINK)
     page.open()
     page.should_be_basket_link()
     page.go_to_basket_view_page()
--- test_product_page.py
+++ test_product_page.py
@@ -1,69 +1,94 @@
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 import pytest
 import faker
 
-# pytest -v --tb=line --language=en test_product_page.py
-LINK = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
-# xfile = 7
-# mask = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer'
-# links = [mask+str(i) for i in range(10) if i != xfile]
-# xlink = pytest.param(mask+str(xfile), marks=pytest.mark.xfail(reason="mistake on page"))
-# links.insert(xfile, xlink)
+MAIN_LINK = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
+REGISTRATION_LINK = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+PRODUCT_LINK = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+
+
+# TODO would be better to make prepare_links() method as fixture
+def prepare_links():
+    xfile = 7
+    mask = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer'
+    links = [mask + str(i) for i in range(10) if i != xfile]
+    xlink = pytest.param(mask + str(xfile), marks=pytest.mark.xfail(reason="mistake on page"))
+    links.insert(xfile, xlink)
+    return links
+
+
+@pytest.mark.need_review
+@pytest.mark.parametrize('link', prepare_links())
+def test_guest_can_add_product_to_basket(browser, link):
+    page = ProductPage(browser, MAIN_LINK)
+    page.open()
+    page.add_to_basket()
+    page.should_be_message()
+    page.should_be_product_name()
+    page.should_be_product_price()
 
 
 @pytest.mark.skip
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    page = ProductPage(browser, LINK)
+    page = ProductPage(browser, MAIN_LINK)
     page.open()
     page.add_to_basket()
     page.should_not_be_success_message()
 
 
-@pytest.mark.skip
+@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
-    page = ProductPage(browser, LINK)
+    page = ProductPage(browser, MAIN_LINK)
     page.open()
     page.add_to_basket()
     page.should_be_success_message()
 
 
-@pytest.mark.from_product_page_to_login_page
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, MAIN_LINK)
     page.open()
     page.should_be_login_link()
 
 
-@pytest.mark.from_product_page_to_login_page
+def test_user_cant_see_success_message(browser):
+    page = ProductPage(browser, MAIN_LINK)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, PRODUCT_LINK)
     page.open()
     page.go_to_login_page()
     page.should_be_login_link()
 
 
-@pytest.mark.login_guest
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    page = BasketPage(browser, PRODUCT_LINK)
+    page.open()
+    page.should_be_basket_link()
+    page.go_to_basket_view_page()
+    page.check_items_in_basket()
+    page.check_text_in_empty_basket()
+
+
+@pytest.mark.need_review
 class TestUserAddToBasketFromProductPage:
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
         email = faker.Faker()
         password = faker.Faker()
-        registration_link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-        registration_page = LoginPage(browser, registration_link)
+        registration_page = LoginPage(browser, REGISTRATION_LINK)
         registration_page.open()
         registration_page.register_new_user(email.email(), password.password())
         registration_page.should_be_authorized_user()
 
-    def test_user_cant_see_success_message(self, browser):
-        page = ProductPage(browser, LINK)
-        page.open()
-        page.should_not_be_success_message()
-
     def test_user_can_add_product_to_basket(self, browser):
-        page = ProductPage(browser, LINK)
+        page = ProductPage(browser, MAIN_LINK)
         page.open()
         page.add_to_basket()
         page.should_be_message()
