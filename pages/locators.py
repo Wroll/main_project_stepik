d5d0de702e08ca2812065ad15ea3f5d276b2dc1e
Wroll <DM.shev4ik@gmail.com>
2020-06-25 20:16:17 +0300

    1

--- pages/locators.py
+++ pages/locators.py
@@ -8,7 +8,7 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
-    EMAIL_INPUT_FOR_REGISTER_FORM = (By.XPATH, "/html/body/div[2]/div/div[2]/div[2]/div/div[2]/form/div[1]/div/input")
+    EMAIL_INPUT_FOR_REGISTER_FORM = (By.CSS_SELECTOR, "[id='id_registration-email']")
     PASSWORD_INPUT_FOR_REGISTER_FORM = (By.NAME, "registration-password1")
     CONFIRM_PASSWORD = (By.NAME, "registration-password2")
     REGISTRATION_SUBMIT = (By.NAME, "registration_submit")
@@ -16,23 +16,23 @@ class LoginPageLocators:
 
 class ProductPageLocators:
     BASKET_BUTTON = (By.CSS_SELECTOR, "form button.btn.btn-lg")
-    MESSAGE_ABOUT_SUCCESS_ADDED_INTO_BASKET = (By.XPATH, "/html/body/div[2]/div/div[1]/div[1]")
+    MESSAGE_ABOUT_SUCCESS_ADDED_INTO_BASKET = (By.XPATH, "//div[@id='messages']/child::div[1]")
     PRODUCT_NAME_SHOULD_BE = (By.CSS_SELECTOR, "div h1")
-    PRODUCT_NAME_ADDED_INTO_BASKET = (By.XPATH, "/html/body/div[2]/div/div[1]/div[1]/div/strong")
-    PRODUCT_PRICE_SHOULD_BE = (By.XPATH, "//*[@id='content_inner']/article/div[1]/div[2]/p[1]")
-    PRODUCT_PRICE_ADDED_INTO_BASKET = (By.XPATH, "/html/body/div[2]/div/div[1]/div[3]/div/p[1]/strong")
-    SUCCESS_MESSAGE = (By.XPATH, "/html/body/div[2]/div/div[1]/div[1]")
+    PRODUCT_NAME_ADDED_INTO_BASKET = (By.CSS_SELECTOR, "#messages div:first-child div.alertinner strong")
+    PRODUCT_PRICE_SHOULD_BE = (By.XPATH, "//p[@class='price_color']")
+    PRODUCT_PRICE_ADDED_INTO_BASKET = (By.XPATH, "//div[@id='messages']/child::div[3]/div/child::p[1]/strong")
+    SUCCESS_MESSAGE = (By.XPATH, "//div[@id='messages']/child::div[1]")
 
 
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
-    VIEW_BASKET_BUTTON = (By.XPATH, "/html/body/header/div[1]/div/div[2]/span/a")
+    VIEW_BASKET_BUTTON = (By.CSS_SELECTOR, "span.btn-group a")
     USER_ICON = (By.CSS_SELECTOR, "div.alertinner.wicon")
 
 
 class ViewBasketLocators:
-    TEXT_ABOUT_EMPTY_BASKET = (By.XPATH, "/html/body/div[2]/div/div[3]/div[2]/p")
-    EMPTY_BASKET = (By.XPATH, "/html/body/div[2]/div/div[3]/div[2]/div[1]")
+    TEXT_ABOUT_EMPTY_BASKET = (By.CSS_SELECTOR, "div[id='content_inner'] p")
+    EMPTY_BASKET = (By.XPATH, "div.basket-title.hidden-xs")
 
 
